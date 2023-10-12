5566b4fb1fe785d8b1d4d71549157106f25047c6
ehek01 <whdgus0515@gmail.com>
2023-10-13 00:07:46 +0900

    UI수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,92 @@ import { Client } from '@stomp/stompjs';
 import ChatUI from "./components/ChatUI";
 
 export default function App() {
-  const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
-  const stompClient = useRef(null);
+    const [ipAddress, setIpAddress] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [inputMessage, setInputMessage] = useState('');
+    const [clients, setClients] = useState([]);  // 추가된 상태
+    const stompClient = useRef(null);
 
-  const serverUrl = process.env.SERVER_URL + '/websocket';
+    const serverUrl = process.env.SERVER_URL + '/websocket';
 
-  useEffect(() => {
-    const socket = new SockJS(serverUrl);
-    stompClient.current = new Client({
-      webSocketFactory: () => socket,
-      reconnectDelay: 5000,
-    });
+    useEffect(() => {
+        const fetchDataAndInitializeWebSocket = async () => {
+            try {
+                const response = await fetch("https://api64.ipify.org?format=json");
+                const data = await response.json();
+                const ipAddress = data.ip;
+                setIpAddress(ipAddress);
+            } catch (error) {
+                console.error("Error fetching IP address:", error);
+            }
 
-    stompClient.current.onConnect = (frame) => {
-        stompClient.current.subscribe('/topic/messages', (messageOutput) => {
-            setMessages((prevMessages) => [...prevMessages, JSON.parse(messageOutput.body)]);
-        });
-    };
+            const socket = new SockJS(serverUrl);
+            stompClient.current = new Client({
+                webSocketFactory: () => socket,
+                reconnectDelay: 5000,
+            });
 
-    stompClient.current.activate();
+            stompClient.current.onConnect = (frame) => {
+                console.log("연결완료")
+                // 기존 메시지 구독 코드
+                stompClient.current.subscribe('/topic/messages', (messageOutput) => {
+                    console.log("서버로부터 메세지가 도착했습니다.")
+                    setMessages((prevMessages) => [...prevMessages, JSON.parse(messageOutput.body)]);
+                });
 
-    return () => {
-      stompClient.current.deactivate();
-    };
-  }, [serverUrl]);
+                stompClient.current.subscribe('/topic/clients', (clientOutput) => {
+                    console.log(clientOutput)
+                    try {
+                        // Parse the received JSON string to an object (here: array)
+                        const receivedClients = JSON.parse(clientOutput.body);
+
+                        // Update the state with the received data
+                        setClients(receivedClients);
+                    } catch (error) {
+                        // Log any error during the JSON parsing to the console
+                        console.error('Error parsing client data:', error);
+                    }
+                });
+            };
+
+            stompClient.current.activate();
+
+            return () => {
+                stompClient.current.deactivate();
+            };
+        };
+
+        fetchDataAndInitializeWebSocket();
+    }, [serverUrl]);
+
 
     const sendMessage = async () => {
-        // IP 주소 수집
-        const response = await fetch("https://api64.ipify.org?format=json");
-        const data = await response.json();
-        const ipAddress = data.ip;
+        if (inputMessage === '') {
+            alert('메세지를 입력해주세요.');
+            return;
+        }
 
-        // 메시지 전송
+        // 메시지 전송 (기존 코드)
         stompClient.current.publish({
             destination: "/app/chat",
             body: JSON.stringify({
                 content: inputMessage,
                 timestamp: new Date().toLocaleTimeString(),
-                ipAddress: ipAddress  // IP 주소 추가
+                ipAddress: ipAddress
             })
         });
 
         setInputMessage('');
     };
 
-  return (
-      <ChatUI
-          inputMessage={inputMessage}
-          setInputMessage={setInputMessage}
-          sendMessage={sendMessage}
-          messages={messages}
-      />
-  );
+    // 클라이언트 상태를 props로 전달
+    return (
+        <ChatUI
+            inputMessage={inputMessage}
+            setInputMessage={setInputMessage}
+            sendMessage={sendMessage}
+            messages={messages}
+            clients={clients}
+        />
+    );
 }
--- src/components/ChatUI.jsx
+++ src/components/ChatUI.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ChatContainer = styled.div`
-  font-family: Arial, sans-serif;
-  max-width: 400px;
+const MainContainer = styled.div`
+  display: flex;
+  max-width: 900px; // Or any desired max-width
+  width: 100%;
+  height: 100vh;
   margin: 0 auto;
-  text-align: center;
+  font-family: Arial, sans-serif;
+`;
+
+const ChatSection = styled.div`
+  flex: 0.7; // takes up 70% of the container width
   padding: 20px;
   background-color: #f8f8f8;
   border-radius: 10px;
   box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
 `;
 
+const ClientSection = styled.div`
+  flex: 0.3; // takes up 30% of the container width
+  background-color: #f0f0f0;
+  padding: 20px;
+  overflow-y: auto;
+  border-left: 1px solid #ccc;
+`;
+
 const ChatHeader = styled.h1`
   color: #e91e63;
   font-size: 24px;
+
+  @media (max-width: 600px) {
+    font-size: 20px;
+  }
 `;
 
 const MessageInput = styled.input`
@@ -23,6 +41,10 @@ const MessageInput = styled.input`
   border: 1px solid #ddd;
   width: 70%;
   margin-right: 10px;
+
+  @media (max-width: 600px) {
+    width: 60%;
+  }
 `;
 
 const SendButton = styled.button`
@@ -33,6 +55,11 @@ const SendButton = styled.button`
   border-radius: 5px;
   cursor: pointer;
   font-size: 16px;
+
+  @media (max-width: 600px) {
+    padding: 8px 16px;
+    font-size: 14px;
+  }
 `;
 
 const MessagesContainer = styled.div`
@@ -43,6 +70,14 @@ const MessagesContainer = styled.div`
   border: 1px solid #ddd;
   border-radius: 5px;
   background-color: #fff;
+
+  @media (max-width: 600px) {
+    max-height: 200px;
+  }
+
+  @media (min-width: 601px) and (max-width: 900px) {
+    max-height: 250px;
+  }
 `;
 
 const Message = styled.div`
@@ -51,41 +86,83 @@ const Message = styled.div`
   padding: 10px;
   border-radius: 5px;
   margin: 5px 0;
+
+  @media (max-width: 600px) {
+    padding: 8px;
+  }
 `;
 
 const MessageWithTimestamp = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > div {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-end;
+  }
 `;
 
+
+
+// IP 주소 마스킹 함수
+const maskIpAddress = (ipAddress) => {
+    const ipParts = ipAddress.split('.');
+    if (ipParts.length === 4) {
+        ipParts[2] = 'x';
+        ipParts[3] = 'x';
+        return ipParts.join('.');
+    }
+    return ipAddress;
+};
+
 // Example usage
-const ChatUI = ({ inputMessage, setInputMessage, sendMessage, messages }) => (
-    <ChatContainer>
-        <ChatHeader>💗Jonghyun2.com에 오신것을 환영합니다.💗</ChatHeader>
-        <div style={{ marginBottom: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <MessageInput
-                type="text"
-                value={inputMessage}
-                onChange={(e) => setInputMessage(e.target.value)}
-                placeholder="Type a message..."
-            />
-            <SendButton onClick={sendMessage}>
-                Send
-            </SendButton>
-        </div>
-        <MessagesContainer>
-            {messages.map((message, index) => (
-                <Message key={index}>
-                    <MessageWithTimestamp>
-                        <span>{message.content}</span>
-                        <span>{message.timestamp}</span>
-                        <span>{message.ipAddress}</span>
-                    </MessageWithTimestamp>
-                </Message>
-            ))}
-        </MessagesContainer>
-    </ChatContainer>
-);
+const ChatUI = ({ inputMessage, setInputMessage, sendMessage, messages, clients }) => {
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage();
+        }
+    };
+
+    return (
+        <MainContainer>
+            <ChatSection>
+                <ChatHeader>💗Jonghyun2.com에 오신것을 환영합니다.💗</ChatHeader>
+                <div style={{ marginBottom: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <MessageInput
+                        type="text"
+                        value={inputMessage}
+                        onChange={(e) => setInputMessage(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                        placeholder="Type a message..."
+                    />
+                    <SendButton onClick={sendMessage}>
+                        Send
+                    </SendButton>
+                </div>
+                <MessagesContainer>
+                    {messages.map((message, index) => (
+                        <Message key={index}>
+                            <MessageWithTimestamp>
+                                <span>{message.content}</span>
+                                <div>
+                                    <span>{message.timestamp}</span>
+                                    {message.ipAddress && <span>{maskIpAddress(message.ipAddress)}</span>}
+                                </div>
+                            </MessageWithTimestamp>
+                        </Message>
+                    ))}
+                </MessagesContainer>
+            </ChatSection>
+            <ClientSection>
+                <h2>Online Clients</h2>
+                {clients.map((client, index) => (
+                    <p key={index}>{client.name}</p>
+                ))}
+            </ClientSection>
+        </MainContainer>
+    );
+};
 
 export default ChatUI;
